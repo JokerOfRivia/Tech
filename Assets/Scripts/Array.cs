cd7b877d1f3e2c3f160aec903b6839b054f5431b
galatsu <tracy4fun.minecraft@gmail.com>
2022-11-08 22:24:37 -0500

    created card array and assigned each card on the scene

---
+++ Assets/Scripts/Array.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array : MonoBehaviour
+{
+
+    public GameObject[] cards; //the array itself; an array of game objects/cards
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
