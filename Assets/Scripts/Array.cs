b503555458f2794e92414743070e7a18ad2cff3f
JokerOfRivia <70218072+JokerOfRivia@users.noreply.github.com>
2022-11-09 18:23:41 -0500

    getting script to work

--- Assets/Scripts/Array.cs
+++ Assets/Scripts/Array.cs
@@ -7,17 +7,39 @@ public class Array : MonoBehaviour
 
     public GameObject[] cards; //the array itself; an array of game objects/cards
 
+
     // Start is called before the first frame update
     void Start()
     {
+
+    }
+    void onClick(){
 
     }
 
     // Update is called once per frame
     void Update()
     {
-    	for (int i=0;i<cards.length-1;i++)
+    	var hit=RaycastHit;
+    	var ray=Camera.main.ScreenPointToRay(input.mousePosition);
+    	for (int i=0;i<cards.length-1;i++){
+    		if(Input.GetMouseButtonDown(0)){
+    			if(Physics.Raycast(ray,hit)){
+    				if(hit.transform.object=cards[i]){
+    					if(cards[i].transform.position.x<1){
+    					while (cards[i].transform.position.x<1){
+    				}
+    				}else if(cards[i].transform.position.x==1){
+    					while (cards[i].transform.position.x>0){
+    				}
+    			}
+    				}
+    			}
+    				
 
-        
-    }
+    		}
+    	}
+	}
+
 }
+
