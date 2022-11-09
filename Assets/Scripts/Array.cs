29c58d4c886f0d72c6a8f25e6c64061f17848566
JokerOfRivia <70218072+JokerOfRivia@users.noreply.github.com>
2022-11-09 17:37:56 -0500

    card script

--- Assets/Scripts/Array.cs
+++ Assets/Scripts/Array.cs
@@ -16,6 +16,7 @@ public class Array : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         
     }
 }
---
+++ Assets/Scripts/Flip.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	OnClick(){
+    		if(flipXValue<1){
+    			flipXValue+=0.2f;
+    		}if else(flipXValue{
+    			flipXValue
+    		}
+    	}	
+    }
+    void OnClick(){
+        if(flipXValue<1){
+    			flipXValue+=0.2f;
+    		}if else(flipXValue{
+    			flipXValue-=0.2f;
+    		}
+ 	}
+}
+
+ 
+
+
